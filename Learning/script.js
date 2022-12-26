ca0095ab6c745211ccd8569e391fa7548dbe9d1b
appu-46 <ap958461@gmail.com>
2022-12-26 20:23:51 +0530

    practising syntax

---
+++ Learning/script.js
@@ -0,0 +1,96 @@
+/*let js = "amazing";
+// if (js === "amazing") alert("JavaScript is FUN!");
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+
+console.log(23);
+
+let firstname = "bablu";
+
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+*/
+
+/*
+let JavaScriptIsFun = false;
+console.log(JavaScriptIsFun);
+
+let year;
+console.log(typeof year)
+
+console.log(typeof JavaScriptIsFun);
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "rthrjryjtr");
+console.log(typeof null);
+*/
+
+/*
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 19990;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastname = "Prajapati";
+console.log(lastname);
+*/
+
+/*
+const now = 2037;
+
+const ageJonas = now - 1991;
+const ageKani = now - 2018;
+
+console.log(ageJonas, ageKani);
+
+console.log(ageJonas * 2, ageKani / 10, 2 ** 3);
+
+const firstName = "Abhishek";
+const lastName = "Prajapati";
+
+console.log(firstName + " " + lastName);
+
+let x = 69 + 69;
+x += 10;
+console.log(x);
+
+console.log(ageJonas < ageKani);
+
+const isFullAge = ageKani >= 18;
+
+*/
+/*
+const now = 2037;
+
+const ageJonas = now - 1991;
+const ageKani = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+const averageAge = (ageJonas + ageKani) / 2;
+console.log(ageJonas, ageKani, averageAge);
+*/
+
+/*
+const heightMark = 1.69;
+const heightJohn = 1.95;
+
+const weightMark = 78;
+const weightJohn = 92;
+
+const markBMI = weightMark / heightMark ** 2;
+const johnBMI = weightJohn / heightJohn ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI, johnBMI, markHigherBMI);
+*/
