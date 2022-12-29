c55101259c06f65d949462be3e8690d1f077ef5d
appu-46 <ap958461@gmail.com>
2022-12-30 01:03:13 +0530

    syncing

---
+++ Learning 2/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+let hasDriversLicense = false;
+
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+*/
+
+function logger() {
+  console.log("My name is Abhishek");
+}
+
+// Calling / invoking / running fucntion
+
+logger();
+logger();
+logger();
