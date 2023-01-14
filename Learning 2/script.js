115c5df98198b47bca5be513ee7643b77771aef6
appu-46 <ap958461@gmail.com>
2023-01-14 12:38:41 +0530

    coding challenge

--- Learning 2/script.js
+++ Learning 2/script.js
@@ -206,6 +206,7 @@ if (abhi[interestedIn]) {
     
 }*/
 
+/*
 const abhi = {
   firstName: "Abhishek",
   lastName: "Prajapati",
@@ -252,4 +253,38 @@ console.log(
   }, and has ${abhi.calcLicense()} driver's license.`
 );
 */
+/*
 console.log(abhi.getSummary());
+*/
+
+/*
+// Coding challenge
+const mark = {
+  firstName: "Mark",
+  lastName: "Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2;
+    return this.BMI;
+  },
+};
+const john = {
+  firstName: "John",
+  lastName: "Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2;
+    return this.BMI;
+  },
+};
+
+console.log(
+  `${
+    john.calcBMI() > mark.calcBMI()
+      ? `${john.firstName}'s BMI (${john.BMI}) is higher than ${mark.firstName}'s BMI (${mark.BMI})`
+      : `${mark.firstName}'s BMI (${mark.BMI}) is higher than ${john.firstName}'s BMI (${john.BMI})`
+  }`
+);
+*/
