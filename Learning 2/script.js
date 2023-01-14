acfff28fbb916142021a214222a5d45eafc7a380
appu-46 <ap958461@gmail.com>
2023-01-14 22:32:28 +0530

    3rd chapter

--- Learning 2/script.js
+++ Learning 2/script.js
@@ -343,3 +343,36 @@ const jonas = [
 
 //////////////////////////////////////////////////////////
 // Coding challenge
+/*
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+function calcTip(bill) {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+}
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(tips[i] + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+function calcAverage(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  const average = sum / arr.length;
+  return average;
+}
+
+console.log(calcAverage(bills));
+console.log(calcAverage(tips));
+console.log(calcAverage(totals));
+*/
