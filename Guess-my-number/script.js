e0ff80eaef804b74b236af2592696c575e72547b
appu-46 <ap958461@gmail.com>
2023-01-26 17:04:51 +0530

    added fucntionality

--- Guess-my-number/script.js
+++ Guess-my-number/script.js
@@ -12,11 +12,27 @@ document.querySelector('.guess').value = 22;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20);
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+
   console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+    score--;
+    document.querySelector('.message').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
+    score--;
+    document.querySelector('.message').textContent = score;
   }
 });
