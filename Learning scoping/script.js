eb544baaef4f180189a5d9fb4c58b09b7869e31b
appu-46 <ap958461@gmail.com>
2023-02-27 23:33:30 +0530

    learnt this command

--- Learning scoping/script.js
+++ Learning scoping/script.js
@@ -26,10 +26,11 @@ const firstName = 'Appu';
 calcAge(1991);
 */
 // Variables
-console.log(me);
+// console.log(me);
 // console.log(job);
 // console.log(year);
 
+/*
 var me = 'Appu';
 let job = 'Developer';
 const year = 1999;
@@ -63,3 +64,37 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+*/
+
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1999);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1980);
+
+const appu = {
+  year: 1999,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+appu.calcAge();
+
+const juliet = {
+  year: 2011,
+};
+
+juliet.calcAge = appu.calcAge;
+
+juliet.calcAge();
