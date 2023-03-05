e137560f6abf2d6792e332d5941cbf7bbf2d39c6
appu-46 <ap958461@gmail.com>
2023-03-05 14:35:54 +0530

    syncing

--- Learning functions deep dive/script.js
+++ Learning functions deep dive/script.js
@@ -1,1 +1,63 @@
 'use strict';
+
+/*
+const bookings = [];
+
+const createBooking = function (
+  fligthNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    fligthNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('PH123', 2, 800);
+createBooking('PH123', 2);
+createBooking('PH123', 7);
+createBooking('PH123', 1000);
+createBooking('PH123', undefined, 1000);
+*/
+/*
+const flight = 'LH123';
+const jonas = {
+  name: 'Jonas Prajapati',
+  passport: 12345435,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 12345435) {
+    alert('Checked in');
+  } else {
+    alert('wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// // Is the same as doing ...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+*/
