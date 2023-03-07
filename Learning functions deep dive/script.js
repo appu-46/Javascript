61431f062029b6b56c0e673c7dc2a8412f18b37f
appu-46 <ap958461@gmail.com>
2023-03-07 13:04:29 +0530

    callng function from a function

--- Learning functions deep dive/script.js
+++ Learning functions deep dive/script.js
@@ -61,3 +61,43 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 */
+
+/*
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!!', upperFirstWord);
+
+transformer('JavaScript is the best!!', oneWord);
+*/
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+const greetArrow = greeting => {
+  name => {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+greetArrow('Hey')('Appu');
