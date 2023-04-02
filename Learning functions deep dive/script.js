2a57956c7f634a92686a4d7dd521ca495d6dcd31
appu-46 <ap958461@gmail.com>
2023-04-02 12:44:17 +0530

    coding challenge

--- Learning functions deep dive/script.js
+++ Learning functions deep dive/script.js
@@ -100,6 +100,7 @@ const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
 greetArrow('Hey')('Appu');
 */
 
+/*
 const lufthansa = {
   airline: 'Lufthansa',
   iataCode: 'LH',
@@ -164,3 +165,58 @@ lufthansa.buyPlane = function () {
 document
   .querySelector('.buy')
   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+const addVat = addTax.bind(null, 0.23);
+// addVat = value => value + value * 0.23
+
+console.log(addVat(100));
+console.log(addVat(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * ratey;
+  };
+};
+
+const addVat2 = addTaxRate(0.23);
+
+console.log(addVat2(100));
+*/
+/*
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1:Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question} \n${this.options.join(
+          '\n'
+        )} \n (Write option number) `
+      )
+    );
+    console.log(answer);
+
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    console.log(this.answers);
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+*/
