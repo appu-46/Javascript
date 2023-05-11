0ca00e95d10505a3f071fed4b59dffce8c016392
appu-46 <ap958461@gmail.com>
2023-05-11 22:45:59 +0530

    code sync

--- Arrays/script.js
+++ Arrays/script.js
@@ -188,6 +188,20 @@ btnTransfer.addEventListener('click', function (e) {
   // console.log(amount, receiverAcc);
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -212,8 +226,7 @@ btnClose.addEventListener('click', function (e) {
 });
 
 /////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+///////////////////////////LECTURES
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
