e5b713b8c041dfd7077d01c8c6053468fdf26632
appu-46 <ap958461@gmail.com>
2023-06-16 20:15:40 +0530

    Difference between ES6 and init

--- OOP/script.js
+++ OOP/script.js
@@ -342,3 +342,33 @@ const Sarah = new StudentCl('Sarah Butt', 1999, 'EXTC');
 console.log(Sarah.introduction());
 Sarah.calcAge();
 */
+
+////////////////////////////////////
+//Inheritence between "Classes" : Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2009, 'EXTC');
+
+jay.introduce();
