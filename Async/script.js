53d1eb07232afc97b5b38a2effa06a258b535da0
appu-46 <ap958461@gmail.com>
2023-06-25 22:03:07 +0530

    added prettier config

--- Async/script.js
+++ Async/script.js
@@ -4,3 +4,17 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+// https://countries-api-836d.onrender.com/countries/
+// https://restcountries.eu/rest/v2/
+// https://restcountries.com/v2/name/console.log();
+
+const request = new XMLHttpRequest();
+
+request.open('GET', 'https://restcountries.com/v2/name/india');
+request.send();
+// console.log(this.responseText);
+
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+});
