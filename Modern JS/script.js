baa6f91134ba2a6b7224db56b3ec84b3a917d8de
appu-46 <ap958461@gmail.com>
2023-08-19 18:07:20 +0530

    code sync

--- Modern JS/script.js
+++ Modern JS/script.js
@@ -1,7 +1,7 @@
 // Importing module
 // import {addToCart, totalPrice as price, tq} from'./shoppingCart.js'
 
-// console.log('Importing Module');
+console.log('Importing Module');
 
 // addToCart('bread', 6)
 
@@ -12,7 +12,19 @@
 // ShoppingCart.addToCart('bread', 7)
 // console.log(ShoppingCart.totalPrice);
 
-import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
 
+import add, { cart } from './shoppingCart.js';
 add('pizza', 2);
-console.log(price);
+add('bread', 5);
+add('apples', 8);
+
+console.log(cart);
+console.log(`Start fetching`);
+const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+const data = await res.json();
+
+console.log(data);
+console.log(`Something`);
--- Modern JS/shoppingCart.js
+++ Modern JS/shoppingCart.js
@@ -1,15 +1,20 @@
-// Importing module 
-console.log('Eporting module')
+// Importing module
+console.log('Eporting module');
 
 const shoppingCart = 10;
-const cart = [];
+export const cart = [];
 
-export const addToCart = function(product, quantity){
-    cart.push({ product, quantity})
-    console.log(`${quantity} ${product} added to cart`);
-}
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
 
 const totalPrice = 237;
-const totalQuantity = 23
+const totalQuantity = 23;
 
-export { totalPrice, totalQuantity as tq}
+export { totalPrice, totalQuantity as tq };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
