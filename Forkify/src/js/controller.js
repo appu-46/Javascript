1c308ff7a0690ab4033353c0a34f85792b752871
appu-46 <ap958461@gmail.com>
2023-10-25 00:34:51 +0530

    code sync

--- Forkify/src/js/controller.js
+++ Forkify/src/js/controller.js
@@ -1,3 +1,11 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+// import icons from '../img/icons.svg'; //Parcel 1
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +19,33 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+    if (!id) return;
+
+    // loading element
+    recipeView.renderSpinner();
+
+    // Loading recipe
+    await model.loadRecipe(id);
+
+    const { recipe } = model.state;
+
+    // rendering Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.error(err);
+    alert(err);
+  }
+};
+
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
+
+// Cleaner way to implment above
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
